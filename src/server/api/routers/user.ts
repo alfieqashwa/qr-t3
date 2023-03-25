adfbc77671527f58ec76514abe080187205f4f1f
alfieqashwa <alfieqashwa@gmail.com>
2023-03-26 05:33:37 +0700

    feat: add API userRoute

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,12 @@
+import { z } from "zod"
+import { createTRPCRouter, protectedProcedure } from "../trpc"
+
+export const userRouter = createTRPCRouter({
+  me: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx }) => {
+      return ctx.prisma.user.findUnique({
+        where: { id: ctx.session.user.id }
+      })
+    }),
+})
