d8627a95fbab4ee782017f895e4dab0c37a80cab
alfieqashwa <alfieqashwa@gmail.com>
2023-03-26 07:42:51 +0700

    fix: change bg-color && create sidebar and navigation menu

--- src/components/LayoutDashboard.tsx
+++ src/components/LayoutDashboard.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react"
 import { Header } from "./Header"
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/router"
+import { Menu, Settings } from "lucide-react"
 
 type LayoutProps = { title?: string; children: ReactNode }
 
@@ -20,8 +21,31 @@ export const LayoutDashboard = ({ title = "Default", children }: LayoutProps): J
   return (
     <>
       <Header titleHeader={titleHeader} />
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {children}
+      <main className="bg-zinc-800 text-zinc-200 grid grid-cols-12">
+        <aside className="bg-zinc-700 min-h-screen col-span-2">
+          <ul>
+            <li>Content</li>
+            <li>Content</li>
+            <li>Content</li>
+            <li>Content</li>
+            <li>Content</li>
+            <li>Content</li>
+          </ul>
+        </aside>
+        <div className="col-span-10">
+          <nav className="py-8 flex items-center">
+            <div className="flex space-x-8 items-center">
+              <Menu className="kurt" />
+              <h1 className="font-semibold text-xl">Event</h1>
+            </div>
+            <input type="text" className="py-2 rounded-lg bg-zinc-900" />
+            <Settings />
+          </nav>
+          <section className="thom bg-zinc-800/80 py-3">
+            <h4>Event / International </h4>
+          </section>
+          {children}
+        </div>
       </main>
     </>
   )
