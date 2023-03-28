64322ea25d0beac5a7ecf0e2c7b06cb3c452569e
alfieqashwa <alfieqashwa@gmail.com>
2023-03-29 06:34:53 +0700

    feat(WIP): create sidebar

--- src/components/LayoutDashboard.tsx
+++ src/components/LayoutDashboard.tsx
@@ -1,8 +1,8 @@
+import { Bell, Codesandbox, Menu, Settings, User } from "lucide-react"
+import { useSession } from "next-auth/react"
+import Image from "next/image"
 import type { ReactNode } from "react"
 import { Header } from "./Header"
-import { useSession } from "next-auth/react"
-import { useRouter } from "next/router"
-import { Menu, Settings } from "lucide-react"
 
 type LayoutProps = { title?: string; children: ReactNode }
 
@@ -10,43 +10,58 @@ export const LayoutDashboard = ({ title = "Default", children }: LayoutProps): J
   const titleHeader = `QR Ticket Concert | ${title}`
 
   const { data: sessionData } = useSession()
-  const router = useRouter()
+  // const router = useRouter()
 
   // console.log(`ROUTER::: `, JSON.stringify(router, null, 2))
 
-  const userEmail = sessionData?.user.email
+  // const userEmail = sessionData?.user.email
+  const userImage = sessionData?.user?.image
   // console.log(userEmail)
 
-
+  console.log(sessionData?.user.image)
   return (
     <>
       <Header titleHeader={titleHeader} />
-      <main className="bg-zinc-800 text-zinc-200 grid grid-cols-12">
-        <aside className="bg-zinc-700 min-h-screen col-span-2">
-          <ul>
-            <li>Content</li>
-            <li>Content</li>
-            <li>Content</li>
-            <li>Content</li>
-            <li>Content</li>
-            <li>Content</li>
-          </ul>
-        </aside>
-        <div className="col-span-10">
-          <nav className="py-8 flex items-center">
-            <div className="flex space-x-8 items-center">
-              <Menu className="kurt" />
-              <h1 className="font-semibold text-xl">Event</h1>
-            </div>
-            <input type="text" className="py-2 rounded-lg bg-zinc-900" />
-            <Settings />
-          </nav>
-          <section className="thom bg-zinc-800/80 py-3">
-            <h4>Event / International </h4>
-          </section>
+      <div className="bg-gradient-to-b from-black to-slate-900 text-zinc-50 relative container max-w-full">
+        <NavigationHeader image={userImage as string} />
+        <Drawer />
+        <main className="ml-[256px] min-h-screen py-4 mx-auto max-w-full">
           {children}
-        </div>
-      </main>
+        </main>
+      </div>
     </>
   )
 }
+
+type NavigationProps = {
+  image?: string
+}
+
+const NavigationHeader = (props: NavigationProps) => (
+  <nav className="h-16 fixed z-50 w-full flex">
+    <section className="flex items-center pr-6 justify-end space-x-8 min-w-[256px] h-16">
+      <div className="flex items-center justify-around w-full">
+        <Codesandbox size={36} />
+        <h2 className="font-bold text-lg">LOGO</h2>
+      </div>
+      <Menu size={32} />
+    </section>
+    <section className="space-x-8 flex items-center w-full justify-end px-8">
+      <Bell />
+      {props.image ?
+        <Image src={props.image} alt="profile" width={32} height={32} className="ring-2 ring-offset rounded-full ring-purple-500" /> : <User />
+      }
+    </section>
+  </nav>
+)
+
+const Drawer = () => (
+  <aside className="bg-gradient-to-b from-zinc-900 to-slate-900 min-h-screen w-[256px] fixed z-30">
+    <ul className="mt-20">
+      <li className="flex px-4 py-3 mx-2 items-center rounded-xl space-x-6 transition duration-150 ease-in-out hover:bg-zinc-800"><Settings /><h3 className="font-semibold tracking-wider">Home</h3></li>
+      <li className="flex px-4 py-3 mx-2 items-center rounded-xl space-x-6 transition duration-150 ease-in-out hover:bg-zinc-800"><Settings /><h3 className="font-semibold tracking-wider">Events</h3></li>
+      <li className="flex px-4 py-3 mx-2 items-center rounded-xl space-x-6 transition duration-150 ease-in-out hover:bg-zinc-800"><Settings /><h3 className="font-semibold tracking-wider">Visitors</h3></li>
+      <li className="flex px-4 py-3 mx-2 items-center rounded-xl space-x-6 transition duration-150 ease-in-out hover:bg-zinc-800"><Settings /><h3 className="font-semibold tracking-wider">Setting</h3></li>
+    </ul>
+  </aside>
+)
