51c32b503c3702e6e742c2335cf7912c07ceab69
alfieqashwa <alfieqashwa@gmail.com>
2023-03-29 20:28:33 +0700

    chore: refactor components

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Calendar, Home, Settings, Users } from "lucide-react";
+import Link from "next/link";
+
+type DrawerProps = {
+  isToggle: boolean;
+};
+
+export const Drawer = ({ isToggle }: DrawerProps) => (
+  <aside
+    className={`fixed z-30 min-h-screen border-r border-slate-900 bg-gradient-to-b from-black to-slate-900 pt-28 ${
+      isToggle ? "w-[128px]" : "w-[256px]"
+    }`}
+  >
+    <ul>
+      <li>
+        <Link
+          href="/dashboard"
+          className="mx-2 flex items-center space-x-6 rounded-xl px-4 py-3 transition duration-150 ease-in-out hover:bg-zinc-800"
+        >
+          <Home className={`${isToggle ? "mx-auto" : ""}`} />
+          {!isToggle && (
+            <h3 className="font-semibold tracking-wider">Dashboard</h3>
+          )}
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/events"
+          className="mx-2 flex items-center space-x-6 rounded-xl px-4 py-3 transition duration-150 ease-in-out hover:bg-zinc-800"
+        >
+          <Calendar className={`${isToggle ? "mx-auto" : ""}`} />
+          {!isToggle && (
+            <h3 className="font-semibold tracking-wider">Events</h3>
+          )}
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/visitors"
+          className="mx-2 flex items-center space-x-6 rounded-xl px-4 py-3 transition duration-150 ease-in-out hover:bg-zinc-800"
+        >
+          <Users className={`${isToggle ? "mx-auto" : ""}`} />
+          {!isToggle && (
+            <h3 className="font-semibold tracking-wider">Visitors</h3>
+          )}
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/settings"
+          className="mx-2 flex items-center space-x-6 rounded-xl px-4 py-3 transition duration-150 ease-in-out hover:bg-zinc-800"
+        >
+          <Settings className={`${isToggle ? "mx-auto" : ""}`} />
+          {!isToggle && (
+            <h3 className="font-semibold tracking-wider">Settings</h3>
+          )}
+        </Link>
+      </li>
+    </ul>
+  </aside>
+);
---
+++ src/components/NavigationHeader.tsx
@@ -0,0 +1,45 @@
+import {
+  Bell,
+  Codesandbox,
+  SidebarClose,
+  SidebarOpen,
+  User,
+} from "lucide-react";
+import Image from "next/image";
+
+type NavigationProps = {
+  image?: string;
+  isToggle: boolean;
+  setIsToggle: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const NavigationHeader = (props: NavigationProps) => (
+  <nav className="border-br fixed z-50 flex h-20 w-full justify-between border-slate-900 bg-gradient-to-br from-slate-800 via-black to-slate-800">
+    <section className="flex h-16 min-w-[256px] items-center justify-end space-x-8 pr-6">
+      <div className="flex w-full items-center justify-around">
+        <Codesandbox size={36} />
+        <h2 className="text-lg font-bold">LOGO</h2>
+      </div>
+      <button
+        className="rounded-full bg-slate-800 p-2.5 transition duration-300 ease-in-out hover:bg-slate-700"
+        onClick={() => props.setIsToggle((t) => !t)}
+      >
+        {props.isToggle ? <SidebarOpen /> : <SidebarClose />}
+      </button>
+    </section>
+    <section className="flex w-full items-center justify-end space-x-8 px-8">
+      <Bell />
+      {props.image ? (
+        <Image
+          src={props.image}
+          alt="profile"
+          width={32}
+          height={32}
+          className="ring-offset rounded-full ring-2 ring-purple-500"
+        />
+      ) : (
+        <User />
+      )}
+    </section>
+  </nav>
+);
