00f6c73580eda3867f6d7123b64f4ba596c3126c
alfieqashwa <alfieqashwa@gmail.com>
2023-03-31 00:07:18 +0700

    fix: animate sidebar

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,95 +1,134 @@
 "use client";
 
+import { motion } from "framer-motion";
+
 import { Calendar, LayoutDashboard, Settings, Users } from "lucide-react";
 import Link from "next/link";
 import useToggleStore from "@/store/useToggle";
 
 export const Drawer = () => {
   const { toggle } = useToggleStore();
+
+  const aside = {
+    initial: { width: 96 },
+    open: {
+      width: 224,
+      transition: {
+        duration: 1,
+        type: "spring",
+      },
+    },
+    close: {
+      width: 96,
+      transition: {
+        duration: 1,
+        type: "spring",
+      },
+    },
+  };
+
+  const head3 = {
+    initial: {
+      fontSize: "0.625rem",
+    },
+    open: {
+      fontSize: "1rem",
+      transition: {
+        duration: 1,
+        type: "spring",
+      },
+    },
+    close: {
+      fontSize: "0.625rem",
+      transition: {
+        duration: 1,
+        type: "spring",
+      },
+    },
+  };
+
   return (
-    <aside
-      className={`fixed z-40 min-h-screen border-r-2 border-slate-700 bg-gradient-to-b from-black to-slate-900 pt-28 transition duration-500 ease-in-out ${
-        toggle ? "" : ""
-      }`}
+    <motion.aside
+      initial="initial"
+      animate={toggle ? "open" : "close"}
+      variants={aside}
+      layout
+      className="fixed z-40 min-h-screen border-r-2 border-slate-700 bg-gradient-to-b from-black to-slate-900 pt-28 transition duration-500 ease-in-out"
     >
-      <ul className="px-1">
-        <li>
-          <Link
-            href="/dashboard"
-            className={`flex items-center rounded-xl py-3 transition duration-150 ease-in-out hover:bg-zinc-800 ${
-              toggle
-                ? "flex-row space-x-6 px-6"
-                : "flex-col justify-end space-y-3 px-2"
-            }`}
+      <div className="px-1">
+        <Link
+          href="/dashboard"
+          className={`flex items-center rounded-xl py-3 transition duration-150 ease-in-out hover:bg-zinc-800 ${
+            toggle
+              ? "flex-row space-x-6 px-6"
+              : "flex-col justify-end space-y-3 px-2"
+          }`}
+        >
+          <LayoutDashboard className="shrink-0" />
+          <motion.h3
+            initial="initial"
+            animate={toggle ? "open" : "close"}
+            variants={head3}
+            className="font-semibold tracking-wider"
           >
-            <LayoutDashboard />
-            <h3
-              className={`font-semibold tracking-wider ${
-                toggle ? "text-base " : "text-[0.625rem]"
-              }`}
-            >
-              Dashboard
-            </h3>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/events"
-            className={`flex items-center rounded-xl py-3 transition duration-150 ease-in-out hover:bg-zinc-800 ${
-              toggle
-                ? "flex-row space-x-6 px-6"
-                : "flex-col justify-end space-y-3 px-2"
-            }`}
+            Dashboard
+          </motion.h3>
+        </Link>
+        <Link
+          href="/events"
+          className={`flex items-center rounded-xl py-3 transition duration-150 ease-in-out hover:bg-zinc-800 ${
+            toggle
+              ? "flex-row space-x-6 px-6"
+              : "flex-col justify-end space-y-3 px-2"
+          }`}
+        >
+          <Calendar className="shrink-0" />
+          <motion.h3
+            initial="initial"
+            animate={toggle ? "open" : "close"}
+            variants={head3}
+            className={`font-semibold tracking-wider`}
           >
-            <Calendar />
-            <h3
-              className={`font-semibold tracking-wider ${
-                toggle ? "text-base" : "text-[0.625rem]"
-              }`}
-            >
-              Events
-            </h3>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/visitors"
-            className={`flex items-center rounded-xl py-3 transition duration-150 ease-in-out hover:bg-zinc-800 ${
-              toggle
-                ? "flex-row space-x-6 px-6"
-                : "flex-col justify-end space-y-3 px-2"
-            }`}
+            Events
+          </motion.h3>
+        </Link>
+        <Link
+          href="/visitors"
+          className={`flex items-center rounded-xl py-3 transition duration-150 ease-in-out hover:bg-zinc-800 ${
+            toggle
+              ? "flex-row space-x-6 px-6"
+              : "flex-col justify-end space-y-3 px-2"
+          }`}
+        >
+          <Users className="shrink-0" />
+          <motion.h3
+            initial="initial"
+            animate={toggle ? "open" : "close"}
+            variants={head3}
+            className="font-semibold tracking-wider"
           >
-            <Users />
-            <h3
-              className={`font-semibold tracking-wider ${
-                toggle ? "text-base" : "text-[0.625rem]"
-              }`}
-            >
-              Visitors
-            </h3>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/settings"
-            className={`flex items-center rounded-xl py-3 transition duration-150 ease-in-out hover:bg-zinc-800 ${
-              toggle
-                ? "flex-row space-x-6 px-6"
-                : "flex-col justify-end space-y-3 px-2"
-            }`}
+            Visitors
+          </motion.h3>
+        </Link>
+        <Link
+          href="/settings"
+          className={`flex items-center rounded-xl py-3 transition duration-150 ease-in-out hover:bg-zinc-800 ${
+            toggle
+              ? "flex-row space-x-6 px-6"
+              : "flex-col justify-end space-y-3 px-2"
+          }`}
+        >
+          <Settings className="shrink-0" />
+          <motion.h3
+            initial="initial"
+            animate={toggle ? "open" : "close"}
+            variants={head3}
+            className="font-semibold tracking-wider"
           >
-            <Settings />
-            <h3
-              className={`font-semibold tracking-wider ${
-                toggle ? "text-base" : "text-[0.625rem]"
-              }`}
-            >
-              Settings
-            </h3>
-          </Link>
-        </li>
-      </ul>
-    </aside>
+            Settings
+          </motion.h3>
+        </Link>
+      </div>
+    </motion.aside>
   );
 };
