327da3afaaeb3f930760691add8270df06ba4911
alfieqashwa <alfieqashwa@gmail.com>
2023-04-23 10:36:31 +0700

    fix: create input ui and submit button

--- src/pages/settings/create-eo.tsx
+++ src/pages/settings/create-eo.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/src/components/ui/Input";
 import { authOptions } from "@/src/server/auth";
 import { api } from "@/src/utils/api";
 import type { GetServerSideProps, NextPage } from "next";
@@ -26,32 +27,53 @@ const CreateEO: NextPage = (): JSX.Element => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="thom mt-8 h-[calc(100vh_-_17vh)]">
-      <div>
-        <label>Name</label>
-        <input className="text-slate-700" name="name" />
-      </div>
-      <div>
-        <label>Phone</label>
-        <input className="text-slate-700" name="phone" />
-      </div>
-      <div>
-        <h3>Address</h3>
-        <div>
-          <label>Street</label>
-          <input className="text-slate-700" name="street" />
-        </div>
-        <div>
-          <label>City</label>
-          <input className="text-slate-700" name="city" />
-        </div>
-        <div>
-          <label>Postal Code</label>
-          <input className="text-slate-700" name="postalCode" />
-        </div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <div className="grid h-screen place-items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="mx-auto flex w-1/3 flex-col justify-center rounded-2xl border-4 border-slate-500 px-16 py-12 shadow-xl"
+      >
+        <h1 className="text-center text-2xl font-bold text-slate-300">
+          Create New Event Organizer
+        </h1>
+        <Input
+          className="mt-10 text-base"
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
+        <Input
+          className="mt-6 text-base"
+          type="text"
+          name="phone"
+          placeholder="Phone"
+        />
+        <Input
+          className="mt-6 text-base"
+          type="text"
+          name="street"
+          placeholder="Street"
+        />
+        <Input
+          className="mt-6 text-base"
+          type="text"
+          name="city"
+          placeholder="City"
+        />
+        <Input
+          className="mt-6 text-base"
+          type="text"
+          name="postalCode"
+          placeholder="Postal Code"
+        />
+        <button
+          className="duration mx-auto mt-12 h-12 w-1/2 rounded-xl border-2 border-slate-400 font-bold text-slate-400 transition hover:border-slate-300 hover:text-slate-300 disabled:cursor-not-allowed disabled:border-slate-700 disabled:text-slate-700"
+          type="submit"
+          disabled
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
