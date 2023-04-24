6dae29564d37a0eea80d8527c16a7ccdcd956ff5
alfieqashwa <alfieqashwa@gmail.com>
2023-04-24 19:39:18 +0700

    fix: change combobox to general component to be reuse

--- src/components/Combobox.tsx
+++ src/components/Combobox.tsx
@@ -19,14 +19,22 @@ import {
 } from "@/components/ui/popover";
 import type { RouterOutputs } from "@/utils/api";
 
-type ProvinceProps = {
-  provinces?: RouterOutputs["address"]["getProvinces"];
+type CommandComboboxProps = {
+  datas?:
+    | RouterOutputs["address"]["getProvinces"]
+    | RouterOutputs["address"]["getRegencies"];
   isLoading: boolean;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
 };
 
-export function ProvincesCombobox({ provinces, isLoading }: ProvinceProps) {
+export function CommandCombobox({
+  datas,
+  isLoading,
+  value,
+  setValue,
+}: CommandComboboxProps) {
   const [open, setOpen] = React.useState(false);
-  const [name, setName] = React.useState("");
 
   isLoading && <p>Loading...</p>;
   return (
@@ -38,10 +46,8 @@ export function ProvincesCombobox({ provinces, isLoading }: ProvinceProps) {
           aria-expanded={open}
           className="mt-6 w-[200px] justify-between"
         >
-          {name
-            ? provinces?.find(
-                (province) => province.name === name.toUpperCase()
-              )?.name
+          {value
+            ? datas?.find((data) => data.name === value.toUpperCase())?.name
             : "Select province..."}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
@@ -51,80 +57,23 @@ export function ProvincesCombobox({ provinces, isLoading }: ProvinceProps) {
           <CommandInput placeholder="Search province..." />
           <CommandEmpty>No framework found.</CommandEmpty>
           <CommandGroup>
-            {provinces?.map((province) => (
+            {datas?.map((data) => (
               <CommandItem
-                key={province.id}
+                key={data.id}
                 onSelect={(currentValue) => {
-                  setName(currentValue === name ? "" : currentValue);
+                  setValue(currentValue === value ? "" : currentValue);
                   setOpen(false);
                 }}
               >
                 <Check
                   className={cn(
                     "mr-2 h-4 w-4",
-                    name.toUpperCase() === province.name
+                    value.toUpperCase() === data.name
                       ? "opacity-100"
                       : "opacity-0"
                   )}
                 />
-                {province.name}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
-  );
-}
-
-type RegenciesProps = {
-  regencies?: RouterOutputs["address"]["getRegencies"];
-  isLoading: boolean;
-};
-
-export function RegenciesCombobox({ regencies, isLoading }: RegenciesProps) {
-  const [open, setOpen] = React.useState(false);
-  const [name, setName] = React.useState("");
-
-  isLoading && <p>Loading...</p>;
-  return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="mt-6 w-[200px] justify-between"
-        >
-          {name
-            ? regencies?.find((regency) => regency.name === name.toUpperCase())
-                ?.name
-            : "Select regencies..."}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] bg-slate-900 p-0">
-        <Command>
-          <CommandInput placeholder="Search province..." />
-          <CommandEmpty>No framework found.</CommandEmpty>
-          <CommandGroup>
-            {regencies?.map((regency) => (
-              <CommandItem
-                key={regency.id}
-                onSelect={(currentValue) => {
-                  setName(currentValue === name ? "" : currentValue);
-                  setOpen(false);
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    name.toUpperCase() === regency.name
-                      ? "opacity-100"
-                      : "opacity-0"
-                  )}
-                />
-                {regency.name}
+                {data.name}
               </CommandItem>
             ))}
           </CommandGroup>
