8337487cb0e4b789354d3b8d767a927701d0f3d3
alfieqashwa <alfieqashwa@gmail.com>
2023-04-27 20:18:32 +0700

    feat: create a sample page where only the user role === DEWA who could enter the page

---
+++ src/pages/dewa/index.tsx
@@ -0,0 +1,61 @@
+import type { GetServerSideProps } from "next";
+import { type NextPage } from "next";
+
+import { H1Title } from "@/components/H1.Title";
+import { Layout } from "@/src/components/layout";
+
+import { authOptions } from "@/server/auth";
+import { prisma } from "@/src/server/db";
+import { Role } from "@prisma/client";
+import { getServerSession } from "next-auth/next";
+
+const title = "Dewa";
+const SettingsPage: NextPage = () => {
+  return (
+    <Layout title={title}>
+      <H1Title title={title} />
+      <div className="mt-4 h-[calc(100vh_-_17vh)]">
+        <h3>{title} is here...</h3>
+      </div>
+    </Layout>
+  );
+};
+
+export default SettingsPage;
+
+// If No Authenticated, then redirect to Home Page. Else, enter this page.
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerSession(ctx.req, ctx.res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { id: session.user.id },
+  });
+
+  const isDewa = user && user.role === Role.DEWA;
+  console.log({ isDewa });
+
+  // sample authorization based on user.role
+  if (!isDewa) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
