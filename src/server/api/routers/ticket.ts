fa0521d4a7b14d8e49155fd47630c20242bb7e14
alfieqashwa <alfieqashwa@gmail.com>
2023-05-24 19:31:54 +0700

    feat: configure sku

--- src/server/api/routers/ticket.ts
+++ src/server/api/routers/ticket.ts
@@ -24,34 +24,52 @@ export const ticketRouter = createTRPCRouter({
   // Mutations
   generate: adminProcedure
     .input(z.object({
-      qty: z.number({
-        required_error: "Qty is required",
-        invalid_type_error: "Qty must be a number",
-      }
-      ).int().gte(10),
-      price: z.number({
-        required_error: "Price is required",
-        invalid_type_error: "Price must be a number",
-      }
-      ).int().gt(0),
+      eventId: z.string({
+        required_error: "EventId is required",
+        invalid_type_error: "EventId must be a string",
+      }).cuid(),
       category: z.string({
         required_error: "Category is required",
         invalid_type_error: "Category must be a string",
       }).min(3).max(15),
-      eventId: z.string({
-        required_error: "EventId is required",
-        invalid_type_error: "EventId must be a string",
-
-      }).cuid()
+      price: z.number({
+        required_error: "Price is required",
+        invalid_type_error: "Price must be a number",
+      }).int().gt(0),
+      qty: z.number({
+        required_error: "Qty is required",
+        invalid_type_error: "Qty must be a number",
+      }).int().gte(10),
     }))
     .mutation(async ({ ctx, input: { qty, price, category, eventId } }) => {
       try {
+        const event = await ctx.prisma.event.findUnique({
+          where: { id: eventId },
+          select: { title: true },
+        })
+        const eventOrganizerId = ctx.session.user.eventOrganizerId as string
+        const eventOrganizer = await ctx.prisma.eventOrganizer.findUnique({
+          where: { id: eventOrganizerId },
+          select: { name: true }
+        })
+
+        // radiohead kid a became rka
+        function abbreviationWords(words: string): string | undefined {
+          if (words == null || typeof words !== "string") return
+          return words.split(" ").map((word) => word.charAt(0)).join("")
+        }
+        const _sku = abbreviationWords(event?.title as string) as string
+        const _eoName = abbreviationWords(eventOrganizer?.name as string) as string
+        if (typeof _sku !== "string" && typeof _eoName !== "string") {
+          throw new Error()
+        }
         function generateTickets() {
-          return Array.from({ length: qty }, () => ({
+          return Array.from({ length: qty }, (_v, i) => ({
             price,
             category,
             eventId,
-            eventOrganizerId: ctx.session.user.eventOrganizerId as string,
+            eventOrganizerId,
+            sku: `${_eoName}-${_sku}-0-${i + 1}`
           }))
         }
         const generatedTickets = generateTickets()
