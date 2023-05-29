31fa35f3a551bfdc3b5818d6f27352f77734d47f
alfieqashwa <alfieqashwa@gmail.com>
2023-05-29 20:11:42 +0700

    fix: it's better approach to handle various possibilities

--- src/components/settings/profile-info/profile-image.tsx
+++ src/components/settings/profile-info/profile-image.tsx
@@ -1,4 +1,5 @@
 import type { User } from "@prisma/client";
+import { User as UserIcon } from "lucide-react";
 import Image from "next/image";
 import type { RouterOutputs } from "~/src/utils/api";
 
@@ -6,12 +7,15 @@ type ProfileImageProps = {
   profile: RouterOutputs["user"]["me"];
 };
 
-export const ProfileImage = ({
-  profile,
-}: ProfileImageProps): JSX.Element | null => {
+export const ProfileImage = ({ profile }: ProfileImageProps): JSX.Element => {
   const { image, name, imageUpdate } = profile as User;
 
-  if (typeof image !== "string" || typeof name !== "string") return null;
+  if (typeof image !== "string" || typeof name !== "string")
+    return (
+      <div className="rounded-full p-4 ring-4 ring-amber-300 ring-offset-2 ring-offset-slate-600">
+        <UserIcon size={128} />
+      </div>
+    );
 
   return (
     <div className="mx-auto">
