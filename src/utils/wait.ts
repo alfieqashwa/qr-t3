43bf7db0c89efc4771338612540a29e16798b551
alfieqashwa <alfieqashwa@gmail.com>
2023-06-04 23:51:42 +0700

    add params and default params of fn wait()

--- src/utils/wait.ts
+++ src/utils/wait.ts
@@ -1,5 +1,5 @@
 /* auto-closed after succeed submit the dialog form */
 
-export function wait() {
-  return new Promise((resolve) => setTimeout(resolve, 1000))
+export function wait(duration = 1000) {
+  return new Promise((resolve) => setTimeout(resolve, duration))
 }
