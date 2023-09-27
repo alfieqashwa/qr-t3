c5ccbc76abddd5c6ebaf99e0d68979c062af679f
Alfie Qashwa <alfieqashwa@gmail.com>
2023-09-27 17:52:35 +0700

    feat: create dynamic event-organizer official website page
    - When slug is not equal to event-organizer name and not equal to path
      '/create-eo', then it will be redirect to 404 page.
    - Setup public official website for each event-organizer, so no need
      authentication to go to the page.

--- src/pages/[slug]/index.tsx
+++ src/pages/[slug]/index.tsx
@@ -1,19 +1,40 @@
-import type { GetServerSideProps, NextPage } from "next"
+import type { GetServerSideProps } from "next"
 import { getServerSession } from "next-auth"
-import { authOptions } from "~/src/server/auth"
+import { authOptions } from "~/server/auth"
+import { prisma } from "~/server/db"
 
-const SlugPage: NextPage = () => {
-  return null
+type Props = { slug: string }
+
+const SlugPage = (props: Props) => {
+  const { slug } = props
+
+  return (
+    <div className="min-h-screen bg-slate-950 p-12">
+      <header className="space-y-3 text-center text-4xl font-bold">
+        <h1 className="uppercase text-amber-300">{slug}</h1>
+        <h2 className="capitalize">Official Website</h2>
+      </header>
+    </div>
+  )
 }
 
 export default SlugPage
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerSession(ctx.req, ctx.res, authOptions)
+  const slugQuery = await prisma.eventOrganizer.findFirst({
+    where: { id: session?.user.eventOrganizerId as string },
+    select: { name: true },
+  })
+
+  const slug = slugQuery?.name as string
 
   const allowedPath = "/create-eo"
 
-  if (ctx.query.slug !== allowedPath)
+  if (
+    ctx.query.slug !== allowedPath &&
+    ctx.query.slug !== slug.replace(/\s+/g, "-")
+  )
     return {
       redirect: {
         destination: "/404",
@@ -21,16 +42,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       },
     }
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    }
-  }
-
   return {
-    props: { session },
+    props: {
+      slug,
+    },
   }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <meta name="description" content="QR Ticket" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-900">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-950">
         <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16 lg:gap-12 ">
           <h2 className="text-md font-semibold text-amber-300">Home</h2>
           <div className="flex flex-col items-center gap-2">
