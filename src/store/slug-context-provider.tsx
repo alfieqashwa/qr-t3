c6381bea060d25150bce55b4117b9cc3e6c0b48d
Alfie Qashwa <alfieqashwa@gmail.com>
2023-09-28 00:12:55 +0700

    fix: add status validation for 'data.slug'

--- src/store/slug-context-provider.tsx
+++ src/store/slug-context-provider.tsx
@@ -13,14 +13,18 @@ export const SlugContext = createContext<SlugContextProvider>({
 })
 
 export const SlugContextProvider = ({ children }: Props) => {
-  const { data } = api.eo.nameBySessionId.useQuery(undefined, {
+  const { data, status } = api.eo.nameBySessionId.useQuery(undefined, {
     select: (data) => ({
       slug: data?.name.replace(/\s+/g, "-"),
     }),
   })
 
+  if (status !== "success") {
+    return
+  }
+
   return (
-    <SlugContext.Provider value={{ slug: data?.slug }}>
+    <SlugContext.Provider value={{ slug: data.slug }}>
       {children}
     </SlugContext.Provider>
   )
