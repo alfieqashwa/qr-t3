d83393cfa88e17b93574a05ad570da0ac7e5bd46
Alfie Qashwa <alfieqashwa@gmail.com>
2023-10-11 20:45:12 +0700

    fix: modify Layout for official website

--- src/components/layout-event-organizer/index.tsx
+++ src/components/layout-event-organizer/index.tsx
@@ -1,11 +1,28 @@
+import { Codesandbox } from "lucide-react"
+import Link from "next/link"
+import { useRouter } from "next/router"
+
 export const LayoutEventOrganizer = ({
   children,
 }: {
   children: React.ReactNode
 }) => {
+  const { query } = useRouter()
+  const slug = query.slug as string
+
   return (
-    <div className="min-h-screen bg-slate-950 p-2 sm:p-4 lg:p-6">
-      <main className="mt-4">{children}</main>
+    <div className="min-h-screen bg-slate-950">
+      <header className="h-14">
+        <nav>
+          <Link
+            href={`/${slug}`}
+            className="flex shrink-0 items-center pl-6 pt-4"
+          >
+            <Codesandbox size={36} className="animate-spin" />
+          </Link>
+        </nav>
+      </header>
+      <main className="px-2 py-4 sm:px-4 lg:px-6">{children}</main>
     </div>
   )
 }
