6a3e272fa75b70921e80181acf5c7715ad5b7bc7
Alfie Qashwa <alfieqashwa@gmail.com>
2023-10-19 17:56:33 +0700

    fix: testing border bugs on production

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class _Document extends Document {
       <Html lang="en">
         <Head />
         <Main />
-        <body className="mx-auto min-h-screen min-w-[350px] max-w-[120rem] bg-background font-sans antialiased">
+        <body className="mx-auto min-h-screen min-w-[350px] max-w-[120rem] font-sans antialiased">
           <NextScript />
         </body>
       </Html>
