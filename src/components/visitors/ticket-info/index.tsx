f0ec8077e6781f9280d3958ca1300896f5b4d2fb
Alfie Qashwa <alfieqashwa@gmail.com>
2023-10-28 23:03:14 +0700

    chore: remove console

--- src/components/visitors/ticket-info/index.tsx
+++ src/components/visitors/ticket-info/index.tsx
@@ -55,140 +55,79 @@ export const TicketInfo = ({ ticket, ticketId }: TicketInfoProps) => {
     ticket.event.date.toDateString() === new Date().toDateString()
   console.log(`isTodayEventYet::: `, isTodaysEventYet)
   return (
-    <>
-      <pre>{JSON.stringify(ticket, null, 2)}</pre>
-      <Card className="min-h-screen min-w-fit px-2 py-8 sm:px-6 lg:px-12">
-        <CardHeader className="text-center">
-          <CardTitle className="md:text-xl">Ticket Information</CardTitle>
-          <CardDescription className="md:text-lg">
-            Ticket ID: {ticket?.id}
-          </CardDescription>
-        </CardHeader>
+    <Card className="min-h-screen min-w-fit px-2 py-8 sm:px-6 lg:px-12">
+      <CardHeader className="text-center">
+        <CardTitle className="md:text-xl">Ticket Information</CardTitle>
+        <CardDescription className="md:text-lg">
+          Ticket ID: {ticket?.id}
+        </CardDescription>
+      </CardHeader>
 
-        <CardContent className="rounded-xl border-2 py-6">
-          <Wrapper title="Event">
-            <p className="uppercase text-amber-300">{ticket?.event?.title}</p>
-            <p>
-              {ticket?.event?.date &&
-                format(ticket.event.date, "PPPP", { locale: id })}
-            </p>
-            <p>
-              <span>Venue: </span>
-              <span className="capitalize">{ticket?.event?.venue}</span>
-            </p>
-            <p>
-              <span className="uppercase text-amber-300">
-                {ticket?.eventOrganizer?.name}
-              </span>
-            </p>
-          </Wrapper>
+      <CardContent className="rounded-xl border-2 py-6 shadow-xl">
+        <Wrapper title="Event">
+          <p className="uppercase text-amber-300">{ticket?.event?.title}</p>
+          <p>
+            {ticket?.event?.date &&
+              format(ticket.event.date, "PPPP", { locale: id })}
+          </p>
+          <p>
+            <span>Venue: </span>
+            <span className="capitalize">{ticket?.event?.venue}</span>
+          </p>
+          <p>
+            <span className="uppercase text-amber-300">
+              {ticket?.eventOrganizer?.name}
+            </span>
+          </p>
+        </Wrapper>
 
-          <Wrapper title="Ticket Info" className="mt-2">
-            <p>
-              Category:{" "}
-              <span className="uppercase text-amber-300">
-                {ticket?.category}
-              </span>
-            </p>
-            <p>
-              Price:{" "}
-              <span className="text-amber-300">
-                {formattedPrice.format(ticket?.price as number)}
-              </span>
-            </p>
-            <p>
-              Status:{" "}
-              <span className="font-bold uppercase text-amber-300">
-                {ticket?.status === "SOLD" && "Purchased"}
-              </span>
-            </p>
-          </Wrapper>
+        <Wrapper title="Ticket Info" className="mt-2">
+          <p>
+            Category:{" "}
+            <span className="uppercase text-amber-300">{ticket?.category}</span>
+          </p>
+          <p>
+            Price:{" "}
+            <span className="text-amber-300">
+              {formattedPrice.format(ticket?.price as number)}
+            </span>
+          </p>
+          <p>
+            Status:{" "}
+            <span className="font-bold uppercase text-amber-300">
+              {ticket?.status === "SOLD" && "Purchased"}
+            </span>
+          </p>
+        </Wrapper>
 
-          <Wrapper title="Visitor" className="mt-2">
-            {ticket?.visitors
-              .filter((t) => t.ticketId === ticketId)
-              .map((v) => (
-                <ul key={v.id} className="flex flex-col items-center">
-                  <li className="capitalize">
-                    <span>Name: </span>
-                    <span className="text-amber-300">{v.name}</span>
-                  </li>
-                  <li>
-                    <span>Phone: </span>
-                    <span className="text-amber-300">{v.phone}</span>
-                  </li>
-                  <li>
-                    <span>Email: </span>
-                    <span className="text-amber-300">{v.email}</span>
-                  </li>
-                  {!isTodaysEventYet ? ( //! TODOS: remove exclamation-mark when production is ready!
-                    <div>
-                      <div className="mt-4 text-center text-xl font-bold">
-                        <p>Today is not the Event Date</p>
-                        <div className="m-2 text-xs text-primary">
-                          <p>Button Check-In & Check-Out should be disabled</p>
-                          <p>
-                            But for the testing development purpose, we enabled
-                            them.
-                          </p>
-                        </div>
-                        {ticket.event?.date && (
-                          <CountdownTimer date={ticket.event?.date} />
-                        )}
-                        <p className="text-base text-rose-400">
-                          TODOS: Setup Start & End Event Time
-                        </p>
-                      </div>
-                      <li className="mt-8 flex items-center justify-center space-x-6">
-                        {/* //? STARTS CHECK-IN */}
-                        <Button
-                          disabled={isLoading || v.isCheckIn}
-                          size="lg"
-                          variant={`${v.isCheckIn ? "destructive" : "default"}`}
-                          onClick={() => handleCheckIn(v.id)}
-                          className="flex items-center justify-center uppercase"
-                        >
-                          <span className="whitespace-nowrap">Check In</span>
-                        </Button>
-                        {/* //? ENDS CHECK-IN */}
-
-                        {/* //? STARTS CHECK-OUT */}
-                        <Button
-                          disabled={isLoading || !v.isCheckIn}
-                          size="lg"
-                          variant={`${
-                            !v.isCheckIn ? "destructive" : "default"
-                          }`}
-                          onClick={() => handleCheckOut(v.id)}
-                          className="flex items-center justify-center uppercase"
-                        >
-                          <span className="whitespace-nowrap">Check Out</span>
-                        </Button>
-                        {/* //? ENDS CHECK-OUT */}
-                      </li>
-
-                      <li className="mt-4 text-center">
-                        <p>
-                          Check In:{" "}
-                          <span className="text-amber-300">
-                            {!!v.checkInDate
-                              ? format(v.checkInDate, "pp")
-                              : "Not Available"}
-                          </span>
-                        </p>
-                        <p>
-                          Check Out:{" "}
-                          <span className="text-amber-300">
-                            {!!v.checkOutDate
-                              ? format(v.checkOutDate, "pp")
-                              : "Not Available"}
-                          </span>
-                        </p>
-                      </li>
-                    </div>
-                  ) : (
+        <Wrapper title="Visitor" className="mt-2">
+          {ticket?.visitors
+            .filter((t) => t.ticketId === ticketId)
+            .map((v) => (
+              <ul key={v.id} className="flex flex-col items-center">
+                <li className="capitalize">
+                  <span>Name: </span>
+                  <span className="text-amber-300">{v.name}</span>
+                </li>
+                <li>
+                  <span>Phone: </span>
+                  <span className="text-amber-300">{v.phone}</span>
+                </li>
+                <li>
+                  <span>Email: </span>
+                  <span className="text-amber-300">{v.email}</span>
+                </li>
+                {!isTodaysEventYet ? ( //! TODOS: remove exclamation-mark when production is ready!
+                  <div>
                     <div className="mt-4 text-center text-xl font-bold">
                       <p>Today is not the Event Date</p>
+                      <div className="m-2 text-xs text-primary">
+                        <p>Button Check-In & Check-Out should be disabled</p>
+                        <p>
+                          But for the testing development purpose, we enabled
+                          them.
+                        </p>
+                      </div>
                       {ticket.event?.date && (
                         <CountdownTimer date={ticket.event?.date} />
                       )}
@@ -196,21 +135,75 @@ export const TicketInfo = ({ ticket, ticketId }: TicketInfoProps) => {
                         TODOS: Setup Start & End Event Time
                       </p>
                     </div>
-                  )}
-                </ul>
-              ))}
+                    <li className="mt-8 flex items-center justify-center space-x-6">
+                      {/* //? STARTS CHECK-IN */}
+                      <Button
+                        disabled={isLoading || v.isCheckIn}
+                        size="lg"
+                        variant={`${v.isCheckIn ? "destructive" : "default"}`}
+                        onClick={() => handleCheckIn(v.id)}
+                        className="flex items-center justify-center uppercase"
+                      >
+                        <span className="whitespace-nowrap">Check In</span>
+                      </Button>
+                      {/* //? ENDS CHECK-IN */}
 
-            <Button
-              variant="secondary"
-              size="lg"
-              className="mt-8"
-              onClick={() => void router.back()}
-            >
-              Go Back
-            </Button>
-          </Wrapper>
-        </CardContent>
-      </Card>
-    </>
+                      {/* //? STARTS CHECK-OUT */}
+                      <Button
+                        disabled={isLoading || !v.isCheckIn}
+                        size="lg"
+                        variant={`${!v.isCheckIn ? "destructive" : "default"}`}
+                        onClick={() => handleCheckOut(v.id)}
+                        className="flex items-center justify-center uppercase"
+                      >
+                        <span className="whitespace-nowrap">Check Out</span>
+                      </Button>
+                      {/* //? ENDS CHECK-OUT */}
+                    </li>
+
+                    <li className="mt-4 text-center">
+                      <p>
+                        Check In:{" "}
+                        <span className="text-amber-300">
+                          {!!v.checkInDate
+                            ? format(v.checkInDate, "pp")
+                            : "Not Available"}
+                        </span>
+                      </p>
+                      <p>
+                        Check Out:{" "}
+                        <span className="text-amber-300">
+                          {!!v.checkOutDate
+                            ? format(v.checkOutDate, "pp")
+                            : "Not Available"}
+                        </span>
+                      </p>
+                    </li>
+                  </div>
+                ) : (
+                  <div className="mt-4 text-center text-xl font-bold">
+                    <p>Today is not the Event Date</p>
+                    {ticket.event?.date && (
+                      <CountdownTimer date={ticket.event?.date} />
+                    )}
+                    <p className="text-base text-rose-400">
+                      TODOS: Setup Start & End Event Time
+                    </p>
+                  </div>
+                )}
+              </ul>
+            ))}
+
+          <Button
+            variant="secondary"
+            size="lg"
+            className="mt-8"
+            onClick={() => void router.back()}
+          >
+            Go Back
+          </Button>
+        </Wrapper>
+      </CardContent>
+    </Card>
   )
 }
