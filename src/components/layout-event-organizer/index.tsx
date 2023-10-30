7f99d76bef5b65bc8461be461a1c6498cc7bd3df
Alfie Qashwa <alfieqashwa@gmail.com>
2023-10-30 23:40:31 +0700

    fix: dashboard btn on dynamic official event page

--- src/components/layout-event-organizer/index.tsx
+++ src/components/layout-event-organizer/index.tsx
@@ -2,6 +2,7 @@ import { Codesandbox } from "lucide-react"
 import { useSession } from "next-auth/react"
 import Link from "next/link"
 import { useRouter } from "next/router"
+import { api } from "~/src/utils/api"
 import { Button } from "~/ui/button"
 import { Copyright } from "../footer"
 
@@ -10,9 +11,19 @@ export const LayoutEventOrganizer = ({
 }: {
   children: React.ReactNode
 }) => {
-  const { query } = useRouter()
-  const slug = query.slug as string
-  const { status } = useSession()
+  const [router, session] = [useRouter(), useSession()]
+  const slug = router.query.slug as string
+  const nameBySessionId = api.eo.nameBySessionId.useQuery(
+    {
+      id: session.data?.user.eventOrganizerId as string,
+    },
+    {
+      enabled: !!session.data?.user.eventOrganizerId,
+      select: (data) => ({
+        slug: data?.name.replace(/\s+/g, "-"),
+      }),
+    },
+  )
 
   return (
     <div className="min-h-screen bg-slate-950">
@@ -24,13 +35,17 @@ export const LayoutEventOrganizer = ({
           >
             <Codesandbox size={36} className="animate-spin" />
           </Link>
-          {status === "authenticated" && (
-            <Link href={`/${slug}/dashboard`} className="pt-1">
-              <Button size="sm" variant="outline">
-                Go to Dashboard
-              </Button>
-            </Link>
-          )}
+          {session.status === "authenticated" &&
+            nameBySessionId.status === "success" && (
+              <Link
+                href={`/${nameBySessionId.data.slug}/dashboard`}
+                className="pt-1"
+              >
+                <Button size="sm" variant="outline">
+                  Go to Dashboard
+                </Button>
+              </Link>
+            )}
         </nav>
       </header>
       <main className="px-2 pb-6 pt-4 sm:px-4 lg:px-6">{children}</main>
