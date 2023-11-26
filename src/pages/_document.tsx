6ff5849159ad34a79c5fa3ff61d9fc11f36e7962
Alfie Qashwa <alfieqashwa@gmail.com>
2023-11-26 22:26:45 +0700

    chore: change Class Component -> Fn Component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,13 @@
-import Document, { Head, Html, Main, NextScript } from "next/document"
+import { Head, Html, Main, NextScript } from "next/document"
 
-export default class _Document extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <Main />
-        <body className="mx-auto min-h-screen min-w-[350px] max-w-[120rem] font-sans antialiased">
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <Main />
+      <body className="mx-auto min-h-screen min-w-[350px] max-w-[120rem] font-sans antialiased">
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
